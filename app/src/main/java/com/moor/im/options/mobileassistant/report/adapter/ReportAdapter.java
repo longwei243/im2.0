c3b77f82b989b741db7219ea198c49d523e2d95a
longwei <liulongwei243@yeah.net>
2016-06-20 18:44:45 +0800

    report

--- app/src/main/java/com/moor/im/options/mobileassistant/report/adapter/ReportAdapter.java
+++ app/src/main/java/com/moor/im/options/mobileassistant/report/adapter/ReportAdapter.java
@@ -2,17 +2,26 @@ package com.moor.im.options.mobileassistant.report.adapter;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.LargeValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.moor.im.R;
@@ -54,7 +63,7 @@ public class ReportAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
         }else if(viewType == ReportData.TYPE_CALL_OUT) {
             return new CallOutViewHolder(LayoutInflater.from(context).inflate(R.layout.report_item_callout, null));
         }else if(viewType == ReportData.TYPE_QUEUE) {
-            return new QueueViewHolder(LayoutInflater.from(context).inflate(R.layout.report_item, null));
+            return new QueueViewHolder(LayoutInflater.from(context).inflate(R.layout.report_item_queue, null));
         }else if(viewType == ReportData.TYPE_IM) {
             return new IMViewHolder(LayoutInflater.from(context).inflate(R.layout.report_item, null));
         }else if(viewType == ReportData.TYPE_SESSION) {
@@ -182,6 +191,88 @@ public class ReportAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
 
         }else if(datas.get(position).type == ReportData.TYPE_QUEUE) {
             ((QueueViewHolder)holder).tv.setText(reportData.name);
+
+            ((QueueViewHolder)holder).report_item_queue_barchart.setPinchZoom(false);
+
+            ((QueueViewHolder)holder).report_item_queue_barchart.setDrawBarShadow(false);
+
+            ((QueueViewHolder)holder).report_item_queue_barchart.setDrawGridBackground(false);
+
+            YAxis leftAxis = ((QueueViewHolder)holder).report_item_queue_barchart.getAxisLeft();
+            leftAxis.setValueFormatter(new LargeValueFormatter());
+            leftAxis.setDrawGridLines(false);
+            leftAxis.setSpaceTop(30f);
+            leftAxis.setAxisMinValue(0f); // this replaces setStartAtZero(true)
+
+            ((QueueViewHolder)holder).report_item_queue_barchart.getAxisRight().setEnabled(false);
+
+            ArrayList<String> xVals = new ArrayList<String>();
+            for (int i = 0; i < 6; i++) {
+                xVals.add((i+1990) + "");
+            }
+
+            ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
+            ArrayList<BarEntry> yVals2 = new ArrayList<BarEntry>();
+            ArrayList<BarEntry> yVals3 = new ArrayList<BarEntry>();
+
+            float mult = 6 * 1000f;
+
+            for (int i = 0; i < 6; i++) {
+                float val = (float) (Math.random() * mult) + 3;
+                yVals1.add(new BarEntry(val, i));
+            }
+
+            for (int i = 0; i < 6; i++) {
+                float val = (float) (Math.random() * mult) + 3;
+                yVals2.add(new BarEntry(val, i));
+            }
+
+            for (int i = 0; i < 6; i++) {
+                float val = (float) (Math.random() * mult) + 3;
+                yVals3.add(new BarEntry(val, i));
+            }
+
+            BarDataSet set1, set2, set3;
+
+            if (((QueueViewHolder)holder).report_item_queue_barchart.getData() != null &&
+                    ((QueueViewHolder)holder).report_item_queue_barchart.getData().getDataSetCount() > 0) {
+                set1 = (BarDataSet)((QueueViewHolder)holder).report_item_queue_barchart.getData().getDataSetByIndex(0);
+                set2 = (BarDataSet)((QueueViewHolder)holder).report_item_queue_barchart.getData().getDataSetByIndex(1);
+                set3 = (BarDataSet)((QueueViewHolder)holder).report_item_queue_barchart.getData().getDataSetByIndex(2);
+                set1.setYVals(yVals1);
+                set2.setYVals(yVals2);
+                set3.setYVals(yVals3);
+                ((QueueViewHolder)holder).report_item_queue_barchart.getData().setXVals(xVals);
+                ((QueueViewHolder)holder).report_item_queue_barchart.getData().notifyDataChanged();
+                ((QueueViewHolder)holder).report_item_queue_barchart.notifyDataSetChanged();
+            } else {
+                // create 3 datasets with different types
+                set1 = new BarDataSet(yVals1, "Company A");
+                // set1.setColors(ColorTemplate.createColors(getApplicationContext(),
+                // ColorTemplate.FRESH_COLORS));
+                set1.setColor(Color.rgb(104, 241, 175));
+                set2 = new BarDataSet(yVals2, "Company B");
+                set2.setColor(Color.rgb(164, 228, 251));
+                set3 = new BarDataSet(yVals3, "Company C");
+                set3.setColor(Color.rgb(242, 247, 158));
+
+                ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
+                dataSets.add(set1);
+                dataSets.add(set2);
+                dataSets.add(set3);
+
+                BarData data = new BarData(xVals, dataSets);
+
+                // add space between the dataset groups in percent of bar-width
+                data.setGroupSpace(80f);
+
+                ((QueueViewHolder)holder).report_item_queue_barchart.setData(data);
+            }
+
+            ((QueueViewHolder)holder).report_item_queue_barchart.invalidate();
+
+
+
         }else if(datas.get(position).type == ReportData.TYPE_IM) {
             ((IMViewHolder)holder).tv.setText(reportData.name);
         }else if(datas.get(position).type == ReportData.TYPE_SESSION) {
@@ -251,10 +342,12 @@ public class ReportAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     }
     class QueueViewHolder extends RecyclerView.ViewHolder implements OnDragVHListener{
         TextView tv;
+        BarChart report_item_queue_barchart;
 
         public QueueViewHolder(View itemView) {
             super(itemView);
             tv = (TextView) itemView.findViewById(R.id.report_item_tv_name);
+            report_item_queue_barchart = (BarChart) itemView.findViewById(R.id.report_item_queue_barchart);
         }
 
         @Override

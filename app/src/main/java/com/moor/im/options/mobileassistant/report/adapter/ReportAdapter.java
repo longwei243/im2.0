a10fd9a3490e65d71ae0b7a2d8d6d8b684662146
longwei <liulongwei243@yeah.net>
2016-06-20 16:21:08 +0800

    report

--- app/src/main/java/com/moor/im/options/mobileassistant/report/adapter/ReportAdapter.java
+++ app/src/main/java/com/moor/im/options/mobileassistant/report/adapter/ReportAdapter.java
@@ -9,9 +9,16 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.moor.im.R;
 import com.moor.im.options.mobileassistant.report.model.ReportData;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,6 +28,12 @@ public class ReportAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
 
     private Context context;
     private List<ReportData> datas;
+
+    private int[] mColors = new int[] {
+            ColorTemplate.VORDIPLOM_COLORS[0],
+            ColorTemplate.VORDIPLOM_COLORS[1],
+            ColorTemplate.VORDIPLOM_COLORS[2]
+    };
 
     public ReportAdapter(Context context, List<ReportData> datas) {
         this.context = context;
@@ -39,7 +52,7 @@ public class ReportAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
         if(viewType == ReportData.TYPE_CALL_IN) {
             return new CallInViewHolder(LayoutInflater.from(context).inflate(R.layout.report_item_callin, null));
         }else if(viewType == ReportData.TYPE_CALL_OUT) {
-            return new CallOutViewHolder(LayoutInflater.from(context).inflate(R.layout.report_item, null));
+            return new CallOutViewHolder(LayoutInflater.from(context).inflate(R.layout.report_item_callout, null));
         }else if(viewType == ReportData.TYPE_QUEUE) {
             return new QueueViewHolder(LayoutInflater.from(context).inflate(R.layout.report_item, null));
         }else if(viewType == ReportData.TYPE_IM) {
@@ -57,9 +70,116 @@ public class ReportAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
         ReportData reportData = datas.get(position);
         if(datas.get(position).type == ReportData.TYPE_CALL_IN) {
             ((CallInViewHolder)holder).tv.setText(reportData.name);
-//            ((CallInViewHolder)holder).report_item_callin_linechart.setData();
+
+            ((CallInViewHolder)holder).report_item_callin_linechart.setDrawGridBackground(false);
+            ((CallInViewHolder)holder).report_item_callin_linechart.setDescription("");
+            ((CallInViewHolder)holder).report_item_callin_linechart.setDrawBorders(false);
+
+            ((CallInViewHolder)holder).report_item_callin_linechart.getAxisLeft().setEnabled(false);
+            ((CallInViewHolder)holder).report_item_callin_linechart.getAxisRight().setDrawAxisLine(false);
+            ((CallInViewHolder)holder).report_item_callin_linechart.getAxisRight().setDrawGridLines(false);
+            ((CallInViewHolder)holder).report_item_callin_linechart.getXAxis().setDrawAxisLine(false);
+            ((CallInViewHolder)holder).report_item_callin_linechart.getXAxis().setDrawGridLines(false);
+
+            // enable touch gestures
+            ((CallInViewHolder)holder).report_item_callin_linechart.setTouchEnabled(true);
+
+            // enable scaling and dragging
+            ((CallInViewHolder)holder).report_item_callin_linechart.setDragEnabled(true);
+            ((CallInViewHolder)holder).report_item_callin_linechart.setScaleEnabled(false);
+
+            // if disabled, scaling can be done on x- and y-axis separately
+            ((CallInViewHolder)holder).report_item_callin_linechart.setPinchZoom(false);
+
+            ArrayList<String> xVals = new ArrayList<String>();
+            for (int i = 0; i < 8; i++) {
+                xVals.add((i) + "");
+            }
+
+            ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+
+            for (int z = 0; z < 2; z++) {
+
+                ArrayList<Entry> values = new ArrayList<Entry>();
+
+                for (int i = 0; i < 8; i++) {
+                    double val = (Math.random() * 8) + 3;
+                    values.add(new Entry((float) val, i));
+                }
+
+                LineDataSet d = new LineDataSet(values, "DataSet " + (z + 1));
+                d.setLineWidth(2.5f);
+                d.setCircleRadius(4f);
+
+                int color = mColors[z % mColors.length];
+                d.setColor(color);
+                d.setCircleColor(color);
+                dataSets.add(d);
+            }
+
+            LineData data = new LineData(xVals, dataSets);
+            ((CallInViewHolder)holder).report_item_callin_linechart.setData(data);
+            ((CallInViewHolder)holder).report_item_callin_linechart.invalidate();
+
+
+
+
+
+
         }else if(datas.get(position).type == ReportData.TYPE_CALL_OUT) {
             ((CallOutViewHolder)holder).tv.setText(reportData.name);
+
+
+            ((CallOutViewHolder)holder).report_item_callout_linechart.setDrawGridBackground(false);
+            ((CallOutViewHolder)holder).report_item_callout_linechart.setDescription("");
+            ((CallOutViewHolder)holder).report_item_callout_linechart.setDrawBorders(false);
+
+            ((CallOutViewHolder)holder).report_item_callout_linechart.getAxisLeft().setEnabled(false);
+            ((CallOutViewHolder)holder).report_item_callout_linechart.getAxisRight().setDrawAxisLine(false);
+            ((CallOutViewHolder)holder).report_item_callout_linechart.getAxisRight().setDrawGridLines(false);
+            ((CallOutViewHolder)holder).report_item_callout_linechart.getXAxis().setDrawAxisLine(false);
+            ((CallOutViewHolder)holder).report_item_callout_linechart.getXAxis().setDrawGridLines(false);
+
+            // enable touch gestures
+            ((CallOutViewHolder)holder).report_item_callout_linechart.setTouchEnabled(true);
+
+            // enable scaling and dragging
+            ((CallOutViewHolder)holder).report_item_callout_linechart.setDragEnabled(true);
+            ((CallOutViewHolder)holder).report_item_callout_linechart.setScaleEnabled(true);
+
+            // if disabled, scaling can be done on x- and y-axis separately
+            ((CallOutViewHolder)holder).report_item_callout_linechart.setPinchZoom(false);
+
+            ArrayList<String> xVals = new ArrayList<String>();
+            for (int i = 0; i < 8; i++) {
+                xVals.add((i) + "");
+            }
+
+            ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+
+            for (int z = 0; z < 2; z++) {
+
+                ArrayList<Entry> values = new ArrayList<Entry>();
+
+                for (int i = 0; i < 8; i++) {
+                    double val = (Math.random() * 8) + 3;
+                    values.add(new Entry((float) val, i));
+                }
+
+                LineDataSet d = new LineDataSet(values, "DataSet " + (z + 1));
+                d.setLineWidth(2.5f);
+                d.setCircleRadius(4f);
+
+                int color = mColors[z % mColors.length];
+                d.setColor(color);
+                d.setCircleColor(color);
+                dataSets.add(d);
+            }
+
+            LineData data = new LineData(xVals, dataSets);
+            ((CallOutViewHolder)holder).report_item_callout_linechart.setData(data);
+            ((CallOutViewHolder)holder).report_item_callout_linechart.invalidate();
+
         }else if(datas.get(position).type == ReportData.TYPE_QUEUE) {
             ((QueueViewHolder)holder).tv.setText(reportData.name);
         }else if(datas.get(position).type == ReportData.TYPE_IM) {
@@ -110,10 +230,13 @@ public class ReportAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     }
     class CallOutViewHolder extends RecyclerView.ViewHolder implements OnDragVHListener{
         TextView tv;
+        LineChart report_item_callout_linechart;
 
         public CallOutViewHolder(View itemView) {
             super(itemView);
             tv = (TextView) itemView.findViewById(R.id.report_item_tv_name);
+            report_item_callout_linechart = (LineChart) itemView.findViewById(R.id.report_item_callout_linechart);
+
         }
 
         @Override
